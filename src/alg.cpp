e45a64822a06b9a4abeb6c55a0698b86a99f2df5
absltn000 <78900100+absltn000@users.noreply.github.com>
2021-04-03 14:45:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,10 +4,8 @@ int cbinsearch(int *arr, int size, int value) {
 int index = -1;
 int number = 0;
 int i = 0, j = size - 1;
-while (i <= j)
-{
-if (arr[i + (j - i + 1) / 2] == value)
-{
+while (i <= j){
+if (arr[i + (j - i + 1) / 2] == value){
 number++;
 index = i + (j - i + 1) / 2;
 break;
@@ -16,17 +14,14 @@ else if (arr[i + (j - i + 1) / 2] < value)
 i = i + (j - i + 1) / 2 + 1;
 else j = i + (j - i + 1) / 2 - 1;
 }
-if (index != -1)
-{
+if (index != -1){
 i = index - 1;
-while ((i >= 0) && (arr[i] == arr[index]))
-{
+while ((i >= 0) && (arr[i] == arr[index])){
 number++;
 i--;
 }
 j = index + 1;
-while ((j < size) && (arr[j] == arr[index]))
-{
+while ((j < size) && (arr[j] == arr[index])){
 number++;
 j++;
 }
